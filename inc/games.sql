b992be299a3a28ef585e8f348aa71c5d341e5b70
dustii94 <34756479+dustii94@users.noreply.github.com>
2018-06-18 18:16:28 -0500

    Added sample sql file

--- inc/games.sql
+++ inc/games.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 18, 2018 at 05:10 PM
+-- Generation Time: Jun 18, 2018 at 05:16 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.2.1
 
